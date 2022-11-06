7ab05616b00b374a1b404842b793ea0a62e99b56
intrapus <60838036+intrapus@users.noreply.github.com>
2022-11-06 23:34:45 +0300

    Update postgres.py

--- postgres.py
+++ postgres.py
@@ -4,14 +4,43 @@ conn = psycopg2.connect(host='localhost', user='postgres',
 password='Master00', 
 dbname='example', port=5432)
 
-cur = conn.cursor()
-cur.execute(
+cursor = conn.cursor()
+
+cursor.execute('DROP TABLE IF EXISTS table2;')
+cursor.execute(
 """
-create table todos (
+create table table2 (
 id serial primary key,
-description varchar not null);
+completed boolean not null);
 """)
 
+cursor.execute('INSERT INTO table2 (id, completed) VALUES (%s, %s);', (1, True))
+
+SQL = 'INSERT INTO table2 (id, completed) VALUES (%(id)s, %(completed)s);'
+
+data = {
+  'id': 2,
+  'completed': False
+}
+cursor.execute(SQL, data)
+
+cursor.execute('SELECT * from table2;')
+result = cursor.fetchall()
+print(result)
+
+cursor.execute('INSERT INTO table2 (id, completed) VALUES (3,True);')
+
+cursor.execute('SELECT * from table2;')
+#---
+result2 = cursor.fetchone()
+print('fetchone ' , result2)
+#---
+result = cursor.fetchmany(2)
+print('fetchmany ' , result)
+#---
+result3 = cursor.fetchone()
+print('fetchone ' , result3)
+
 conn.commit()
-cur.close()
+cursor.close()
 conn.close()
