8d605a45cac2e18c35952b5c86ade6e4ad4c7e1e
intrapus <60838036+intrapus@users.noreply.github.com>
2022-11-13 02:42:12 +0300

    test

--- app.py
+++ app.py
@@ -4,8 +4,19 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:mysecretpassword@192.168.13.201:5432/example'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+class Person(db.Model):
+  __tablename__ = 'persons'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(), nullable=False)
+   
+with app.app_context():
+    db.create_all()
+
 @app.route('/')
+
 def index():
-  return "Hello World"
+    person = Person.query.first()
+    return 'Hello ' + person.name 
